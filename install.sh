41cbe428bdcf00eeb1a95cf61df0f2ff5b43a2e4
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-11-04 18:38:13 +0100

    refactor: ♻️ Use espup instead of the bash script

--- install.sh
+++ install.sh
@@ -2,42 +2,23 @@
 
 set -eu
 
-
-RE_VERSION="^[0-9]+\.[0-9]+(\.[0-9]+)?(\.[0-9]+)?$"
-
-function check_version_formatting() {
-  if [[ ! "${1}" =~ $RE_VERSION ]];
-  then
-    echo "ERROR: version number is not correctly formatted: ${1}"
-    exit 1
-  fi
-}
-
 function format_version() {
   ver=$1
 
   # The number of iterations to perform is 3 (the maximum occurences of '.' in
   # a version string) minus the number of occurences of '.' already present in
   # the version string.
-  res=${ver//[^.]}
+  res=${ver//[^.]/}
   iters=$((3 - ${#res}))
 
   # Append '.0' to the version string $iters number of times to create a
   # version string which matches the release tags.
-  for ((i = 0; i < iters; i++));
-  do
+  for ((i = 0; i < iters; i++)); do
     ver="${ver}.0"
   done
 
   echo "$ver"
 }
-
-function latest_version() {
-  curl -s https://api.github.com/repos/esp-rs/rust-build/releases \
-    | jq -r "map(select(.prerelease | not)) | map(.tag_name) | first" \
-    | sed -e "s/^v//"
-}
-
 
 # Download and execute the installation script from the esp-rs/rust-build
 # repository: https://github.com/esp-rs/rust-build
@@ -50,45 +31,28 @@ function latest_version() {
 # install that version of the compiler.
 
 crates="${1:-}"
-version="${2:-latest}"
+version="${2-}"
 buildtargets="${3:-all}"
 
-case $version in
-  latest)
-    version=$(latest_version)
-    ;;
-
-  *)
-    version=$(format_version "$version")
-    ;;
-esac
-
-check_version_formatting "$version"
-
 curl \
-  -L "https://github.com/esp-rs/rust-build/releases/download/v$version/install-rust-toolchain.sh" \
-  -o "$HOME/install-rust-toolchain.sh"
+  -L "https://github.com/esp-rs/espup/releases/latest/download/espup-x86_64-unknown-linux-gnu" \
+  -o "$HOME/espup"
 
-chmod +x "$HOME/install-rust-toolchain.sh"
+chmod a+x "$HOME/espup"
 
-function install_rust_toolchain() {
-  "$HOME/install-rust-toolchain.sh" \
+if [[ -n "${version}" ]]; then
+  version=$(format_version "$version")
+  "$HOME/espup" install \
+    --export-file "$HOME/exports" \
+    --toolchain-version "$version" \
+    --extra-crates "$crates" \
+    --build-target "${buildtargets}"
+else
+  "$HOME/espup" install \
     --export-file "$HOME/exports" \
     --extra-crates "$crates" \
-    --build-target "${buildtargets}" \
-    "@"
-}
-
-case $version in
-  latest)
-    install_rust_toolchain
-    ;;
-
-  *)
-    install_rust_toolchain --toolchain-version "$version"
-    ;;
-esac
-
+    --build-target "${buildtargets}"
+fi
 
 # With the required exports specified by the rust-build installation script
 # saved to a file, we can simply `source` it to export said variables. GitHub
@@ -98,5 +62,5 @@ esac
 
 #shellcheck source=/dev/null
 source "$HOME/exports"
-echo "$PATH" >> "$GITHUB_PATH"
-echo "LIBCLANG_PATH=${LIBCLANG_PATH}" >> "$GITHUB_ENV"
+echo "$PATH" >>"$GITHUB_PATH"
+echo "LIBCLANG_PATH=${LIBCLANG_PATH}" >>"$GITHUB_ENV"
