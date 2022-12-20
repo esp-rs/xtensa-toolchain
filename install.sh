0b7f70e54f0ed17d8c3fec52b52e90a635e84f4a
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2022-12-20 14:04:16 +0100

    Use `espup` (#16)
    * refactor: ♻️ Use espup instead of rust-build script
    
    * chore: 🎨 Add name to step
    
    * refactor: ♻️ Avoid using install.sh
    
    * chore: 🔥 Remove install.sh
    
    * docs: 📝 Update readme
    
    * refactor: ♻️ Use curl with header argument
    
    * ci: 🐛 Fix permission issues
    
    * ci: 🐛 Add shell property

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 
 set -eu
 
-
 RE_VERSION="^[0-9]+\.[0-9]+(\.[0-9]+)?(\.[0-9]+)?$"
 
 function check_version_formatting() {
-  if [[ ! "${1}" =~ $RE_VERSION ]];
-  then
+  if [[ ! "${1}" =~ $RE_VERSION ]]; then
     echo "ERROR: version number is not correctly formatted: ${1}"
     exit 1
   fi
@@ -19,13 +17,12 @@ function format_version() {
   # The number of iterations to perform is 3 (the maximum occurences of '.' in
   # a version string) minus the number of occurences of '.' already present in
   # the version string.
-  res=${ver//[^.]}
+  res=${ver//[^.]/}
   iters=$((3 - ${#res}))
 
   # Append '.0' to the version string $iters number of times to create a
   # version string which matches the release tags.
-  for ((i = 0; i < iters; i++));
-  do
+  for ((i = 0; i < iters; i++)); do
     ver="${ver}.0"
   done
 
@@ -33,15 +30,26 @@ function format_version() {
 }
 
 function latest_version() {
-  curl -s https://api.github.com/repos/esp-rs/rust-build/releases \
-    | jq -r "map(select(.prerelease | not)) | map(.tag_name) | first" \
-    | sed -e "s/^v//"
+  curl -s https://api.github.com/repos/esp-rs/rust-build/releases -H 'Authorization: $GITHUB_TOKEN' |
+    jq -r "map(select(.prerelease | not)) | map(.tag_name) | first" |
+    sed -e "s/^v//"
 }
 
+buildtargets="${1:-all}"
+version="${2:-latest}"
 
-# Download and execute the installation script from the esp-rs/rust-build
-# repository: https://github.com/esp-rs/rust-build
-#
+case $version in
+latest)
+  version=$(latest_version)
+  ;;
+
+*)
+  version=$(format_version "$version")
+  ;;
+esac
+
+check_version_formatting "$version"
+
 # This installs not only the Rust compiler fork with support for the Xtensa
 # architecture but also the required Xtensa toolchain binaries. We save the
 # required exports to a file for later processing, to handle around some
@@ -49,46 +57,7 @@ function latest_version() {
 # formatted following (our extended version of) semver is provided, attempt to
 # install that version of the compiler.
 
-crates="${1:-}"
-version="${2:-latest}"
-buildtargets="${3:-all}"
-
-case $version in
-  latest)
-    version=$(latest_version)
-    ;;
-
-  *)
-    version=$(format_version "$version")
-    ;;
-esac
-
-check_version_formatting "$version"
-
-curl \
-  -L "https://github.com/esp-rs/rust-build/releases/download/v$version/install-rust-toolchain.sh" \
-  -o "$HOME/install-rust-toolchain.sh"
-
-chmod +x "$HOME/install-rust-toolchain.sh"
-
-function install_rust_toolchain() {
-  "$HOME/install-rust-toolchain.sh" \
-    --export-file "$HOME/exports" \
-    --extra-crates "$crates" \
-    --build-target "${buildtargets}" \
-    "@"
-}
-
-case $version in
-  latest)
-    install_rust_toolchain
-    ;;
-
-  *)
-    install_rust_toolchain --toolchain-version "$version"
-    ;;
-esac
-
+$HOME/.cargo/bin/espup install -l debug --export-file $HOME/exports --targets ${buildtargets} --toolchain-version $version
 
 # With the required exports specified by the rust-build installation script
 # saved to a file, we can simply `source` it to export said variables. GitHub
@@ -98,5 +67,5 @@ esac
 
 #shellcheck source=/dev/null
 source "$HOME/exports"
-echo "$PATH" >> "$GITHUB_PATH"
-echo "LIBCLANG_PATH=${LIBCLANG_PATH}" >> "$GITHUB_ENV"
+echo "$PATH" >>"$GITHUB_PATH"
+echo "LIBCLANG_PATH=${LIBCLANG_PATH}" >>"$GITHUB_ENV"
